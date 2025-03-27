fd8efb757e5dd62e37d3d37ba46578db81decb8b
sarav <mkmohankumar261998@gmail.com>
2025-03-27 22:35:24 +0530

    Signup and Login Created and fatching test doen

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
 const Signup = () => {
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({ userEmail: "", password: "" , userName: ""});
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.email || !formData.password) {
+    if (!formData.userEmail || !formData.password || !formData.userName) {
       setError("All fields are required!");
       return;
     }
-    setError("");
+    setError(null);
+    const sendUser = await fetch('http://localhost:5500/auth/newuser', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ formData }),
+      });
+    const parseResp = await sendUser.json();
+    console.log(parseResp)
     alert("Signup Successful!");
   };
 
@@ -24,9 +31,18 @@ const Signup = () => {
         <h2 className="text-2xl font-bold text-center text-blue-600">Signup</h2>
         {error && <p className="text-red-500 text-center mt-2">{error}</p>}
         <form onSubmit={handleSubmit} className="mt-4">
+        <input
+            type="text"
+            name="userName"
+            placeholder="Username"
+            value={formData.userName}
+            onChange={handleChange}
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
+          />
+
           <input
             type="email"
-            name="email"
+            name="userEmail"
             placeholder="Email"
             value={formData.email}
             onChange={handleChange}
