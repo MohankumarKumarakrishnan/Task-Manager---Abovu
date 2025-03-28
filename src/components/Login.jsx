8ae6e613967eea0f3b541398fbfbca47832e471e
sarav <mkmohankumar261998@gmail.com>
2025-03-28 12:08:17 +0530

    JWT Authentication Completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
+import {useNavigate} from "react-router-dom"; 
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
-
+  const [error, setError] = useState(null);
+  const navigate = useNavigate()
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) =>  {
     e.preventDefault();
     if (!formData.email || !formData.password) {
       setError("All fields are required!");
       return;
     }
-    setError("");
+    setError(null);
+    console.log(formData.email, formData.password)
+    const sendUser = await fetch('http://localhost:5500/auth/login', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ formData }),
+    });
+  const parseResp = await sendUser.json();
+  console.log(parseResp)
     alert("Login Successful!");
+    navigate("/")
   };
 
   return (
